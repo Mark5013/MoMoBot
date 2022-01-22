fc3f5dc2e764eba89595b6ca4bbaf0af92078141
Mark5013 <mark1mcennis@gmail.com>
2022-01-22 15:24:02 -0500

    commit

--- commands/weather.js
+++ commands/weather.js
@@ -25,49 +25,58 @@ module.exports = {
         if(name === 'city') {
             //gets the name of the city that the user inputted 
             const city  = interaction.options.getString('place');
-            if(city === " ") { interaction.reply("Please enter a valid city name!")}
             //if(interaction.getString('scale') !== 'F' || 'C' || 'f' || 'c') { interaction.reply('Invalid temperature scale') };
             weather.find({search: `${city}`, degreeType: 'F'}, function(err, result) {
                 if(err) {
                     console.log(err);
                     interaction.reply('Something went wrong!');
-                } 
-                //creates embed with weather data
-                const cityEmbed = new MessageEmbed()
-                    .setColor('#0099ff')
-                    .setTitle(`${result[0].location.name} Weather Report`)
-                    .setThumbnail(`${result[0].current.imageUrl}`)
-                    .addFields(
-                        {name: 'Temperature', value: `${result[0].current.temperature}`},
-                        {name: '\u200B', value: '\u200B'},
-                        {name: 'Weather', value: `${result[0].current.skytext}`, inline: true},
-                        {name: 'Humidity', value: `${result[0].current.humidity}`, inline: true},
-                        {name: 'Wind Speed', value: `${result[0].current.windspeed}`, inline: true});
+                }
+                console.log(result.length);
+                console.log(result);
+                if(result.length === 0 ) { 
+                    interaction.reply("Invalid City name!");
+                } else{
+                    //creates embed with weather data
+                    const cityEmbed = new MessageEmbed()
+                        .setColor('#0099ff')
+                        .setTitle(`${result[0].location.name} Weather Report`)
+                        .setThumbnail(`${result[0].current.imageUrl}`)
+                        .addFields(
+                            {name: 'Temperature', value: `${result[0].current.temperature}`},
+                            {name: '\u200B', value: '\u200B'},
+                            {name: 'Weather', value: `${result[0].current.skytext}`, inline: true},
+                            {name: 'Humidity', value: `${result[0].current.humidity}`, inline: true},
+                            {name: 'Wind Speed', value: `${result[0].current.windspeed}`, inline: true});
                 //sends the embed
                 interaction.reply({ embeds: [cityEmbed] });
+                }
             })
         //checks if the zipcode subcommand was chosen
         } else if(name === 'zipcode') {
             //gets the zipcode that was entered by the user
             const zip = interaction.options.getString('zip');
-            weather.find({search: `${zip}`, degreeType: `${interaction.getString('tempscale')}`}, function(err, result) {
+            weather.find({search: `${zip}`, degreeType: 'F'}, function(err, result) {
                 if(err) { 
                     console.log(err)
                     interaction.reply('Something went wrong!');
                 }
-                //creates embed with weather data
-                const zipEmbed = new MessageEmbed()
-                    .setColor('#0099ff')
-                    .setTitle(`${result[0].location.name} Weather Report`)
-                    .setThumbnail(`${result[0].current.imageUrl}`)
-                    .addFields(
-                        {name: 'Temperature', value: `${result[0].current.temperature}`},
-                        {name: '\u200B', value: '\u200B'},
-                        {name: 'Weather', value: `${result[0].current.skytext}`, inline: true},
-                        {name: 'Humidity', value: `${result[0].current.humidity}`, inline: true},
-                        {name: 'Wind Speed', value: `${result[0].current.windspeed}`, inline:true});
-                //sends embed
-                interaction.reply({ embeds: [zipEmbed] });
+                if(result.length === 0) { 
+                    interaction.reply("Invalid zip code!")
+                } else {
+                    //creates embed with weather data
+                    const zipEmbed = new MessageEmbed()
+                        .setColor('#0099ff')
+                        .setTitle(`${result[0].location.name} Weather Report`)
+                        .setThumbnail(`${result[0].current.imageUrl}`)
+                        .addFields(
+                            {name: 'Temperature', value: `${result[0].current.temperature}`},
+                            {name: '\u200B', value: '\u200B'},
+                            {name: 'Weather', value: `${result[0].current.skytext}`, inline: true},
+                            {name: 'Humidity', value: `${result[0].current.humidity}`, inline: true},
+                            {name: 'Wind Speed', value: `${result[0].current.windspeed}`, inline:true});
+                    //sends embed
+                    interaction.reply({ embeds: [zipEmbed] });
+                }
             })
         //if neither is chosen, this will execute
         } else {
